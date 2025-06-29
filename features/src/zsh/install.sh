cc541d478c1c3fc78e5357acc7d99bbdd589db98
Francois Seguin <admin@ghostmind.dev>
2025-06-29 23:29:34 +0000

    updating

--- features/src/zsh/install.sh
+++ features/src/zsh/install.sh
@@ -176,11 +176,13 @@ echo "Setting environment variables..."
 # Set theme environment variable
 THEME_TO_SET="${INIT_ZSH_THEME:-${THEME}}"
 echo "INIT_ZSH_THEME=\"${THEME_TO_SET}\"" >> /etc/environment
+echo "export INIT_ZSH_THEME=\"${THEME_TO_SET}\"" >> /etc/zsh/zshenv
 echo "Environment variable INIT_ZSH_THEME set to: ${THEME_TO_SET}"
 
 # Set plugins environment variable
 PLUGINS_TO_SET="${INIT_ZSH_PLUGINS:-${HARDCODED_PLUGINS}}"
 echo "INIT_ZSH_PLUGINS=\"${PLUGINS_TO_SET}\"" >> /etc/environment
+echo "export INIT_ZSH_PLUGINS=\"${PLUGINS_TO_SET}\"" >> /etc/zsh/zshenv
 echo "Environment variable INIT_ZSH_PLUGINS set to: ${PLUGINS_TO_SET}"
 
 # Ensure environment variables are available in current session
